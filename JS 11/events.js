01a22f94135a9bbf78058cc78bf96881503ab089
dannycarter <dannycarter@cox.net>
2023-05-02 07:35:18 -0700

    Update

--- JS 11/events.js
+++ JS 11/events.js
@@ -2,6 +2,6 @@ var button = document.getElementById("my-button");
   //button.addEventListener("click", function() {
   //button.addEventListener("mouseover", function() {
   //button.addEventListener("mouseot", function() {
-    button.addEventListener("keyup", function() {
-    alert("Button was clicked!");
+     button.addEventListener("keyup", function() {
+     alert("Button was clicked!");
 });
